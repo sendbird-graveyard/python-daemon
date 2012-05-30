fad98d99ab2345d437b2075094971ee62cc6fcfe
Ben Finney <ben+python@benfinney.id.au>
2012-05-30 10:06:33 +1000

    Update copyright notices.

--- test/test_daemon.py
+++ test/test_daemon.py
@@ -3,7 +3,7 @@
 # test/test_daemon.py
 # Part of ‘python-daemon’, an implementation of PEP 3143.
 #
-# Copyright © 2008–2011 Ben Finney <ben+python@benfinney.id.au>
+# Copyright © 2008–2012 Ben Finney <ben+python@benfinney.id.au>
 #
 # This is free software: you may copy, modify, and/or distribute this work
 # under the terms of the Apache License, version 2.0 as published by the
