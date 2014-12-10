f7b70f5a232e578580baaa14a5475b6195d8d228
Ben Finney <ben@benfinney.id.au>
2014-12-10 18:29:17 +1100

    Declare a maximum compatible version of ‘lockfile’ library.

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
             ],
         install_requires=[
             "setuptools",
-            "lockfile >=0.7",
+            "lockfile >=0.7,<0.9",
             ],
 
         # PyPI metadata.
